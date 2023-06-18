714da049008f5b53f4f0a4a4021557e5efd2f4b8
Hakim <shanun036@gmail.com>
2023-06-18 18:49:34 +0700

    masih Bug

--- app/src/main/java/com/irnhakim/myapplication/ui/location/LocationFragment.java
+++ app/src/main/java/com/irnhakim/myapplication/ui/location/LocationFragment.java
@@ -1,6 +1,8 @@
 package com.irnhakim.myapplication.ui.location;
 
-
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,132 +10,134 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
 import com.irnhakim.myapplication.R;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+public class LocationFragment extends Fragment {
 
-public class LocationFragment extends Fragment implements OnMapReadyCallback {
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            getCurrentLocation(googleMap);
+        }
+    };
 
-    private GoogleMap googleMap;
-    private final List<NearbyPlace> nearbyPlaces = new ArrayList<>();
-    private static final int MAX_MARKERS = 5;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private LocationCallback locationCallback;
+    private PlacesClient placesClient;
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
 
     @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_location, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
-            mapFragment.getMapAsync(this);
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
+            Places.initialize(requireContext(), getString(R.string.google_maps_key));
+            placesClient = Places.createClient(requireContext());
+            mapFragment.getMapAsync(callback);
         }
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        this.googleMap = googleMap;
-        LatLng sydney = new LatLng(-34, 151);
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    private void getCurrentLocation(GoogleMap googleMap) {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+            return;
+        }
 
-        performApiCall("restaurant");
-        performApiCall("cafe");
+        googleMap.setMyLocationEnabled(true);
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000); // Update location every 5 seconds
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult == null) {
+                    return;
+                }
+
+                for (Location location : locationResult.getLocations()) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    googleMap.addMarker(new MarkerOptions().position(latLng).title("Current Location"));
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15f));
+
+                    findNearbyPlaces(location, googleMap);
+                }
+            }
+        };
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
     }
 
-    private void performApiCall(String placeType) {
-        String apiKey = "YOUR_API_KEY";
-        int radius = 1000; // Radius in meters
-        LatLng currentLatLng = new LatLng(-34, 151); // Update with your current location
+    private void findNearbyPlaces(Location location, GoogleMap googleMap) {
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.NAME, Place.Field.LAT_LNG);
 
-        String location = currentLatLng.latitude + "," + currentLatLng.longitude;
+        FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(placeFields).build();
 
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        placesClient.findCurrentPlace(request).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                FindCurrentPlaceResponse response = task.getResult();
 
-        OkHttpClient httpClient = new OkHttpClient.Builder()
-                .addInterceptor(loggingInterceptor)
-                .build();
+                if (response != null) {
+                    List<PlaceLikelihood> placeLikelihoods = response.getPlaceLikelihoods();
+                    List<Marker> markers = new ArrayList<>();
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://maps.googleapis.com/maps/api/")
-                .client(httpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
+                    for (int i = 0; i < Math.min(placeLikelihoods.size(), 5); i++) {
+                        PlaceLikelihood placeLikelihood = placeLikelihoods.get(i);
+                        Place place = placeLikelihood.getPlace();
+                        LatLng placeLatLng = place.getLatLng();
 
-        PlacesApiService apiService = retrofit.create(PlacesApiService.class);
-        Call<ApiResponse> call = apiService.getNearbyPlaces(location, radius, placeType, apiKey);
-        call.enqueue(new Callback<ApiResponse>() {
-            @Override
-            public void onResponse(Call<ApiResponse> call, Response<ApiResponse> response) {
-                if (response.isSuccessful()) {
-                    ApiResponse apiResponse = response.body();
-                    if (apiResponse != null && apiResponse.getResults() != null) {
-                        List<Result> results = apiResponse.getResults();
-                        for (Result result : results) {
-                            if (nearbyPlaces.size() >= MAX_MARKERS) {
-                                break;
-                            }
-
-                            Geometry geometry = result.getGeometry();
-                            Location location = geometry.getLocation();
-                            String placeName = result.getName();
-
-                            double latitude = location.getLat();
-                            double longitude = location.getLng();
-
-                            NearbyPlace nearbyPlace = new NearbyPlace(latitude, longitude, placeName);
-                            nearbyPlaces.add(nearbyPlace);
+                        if (placeLatLng != null) {
+                            Marker marker = googleMap.addMarker(new MarkerOptions()
+                                    .position(placeLatLng)
+                                    .title(place.getName()));
+                            markers.add(marker);
                         }
                     }
                 }
-
-                addMarkersForNearbyPlaces();
-            }
-
-            @Override
-            public void onFailure(Call<ApiResponse> call, Throwable t) {
-                // Handle API call failure
             }
         });
     }
 
-    private void addMarkersForNearbyPlaces() {
-        if (googleMap == null) {
-            return;
-        }
-
-        googleMap.clear();
-
-        for (NearbyPlace nearbyPlace : nearbyPlaces) {
-            LatLng placeLatLng = new LatLng(nearbyPlace.getLatitude(), nearbyPlace.getLongitude());
-            String placeName = nearbyPlace.getName();
-
-            MarkerOptions markerOptions = new MarkerOptions()
-                    .position(placeLatLng)
-                    .title(placeName);
-
-            googleMap.addMarker(markerOptions);
-        }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
     }
 }
